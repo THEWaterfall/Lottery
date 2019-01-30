8326aed319e73fffd3d19918b7d67b9c67b8a8cd
THEWaterfall <floodlight98@gmail.com>
2019-01-30 22:46:37 +0200

    Added checking IP on login attempt
    If IP is blocked, exception is thrown.

--- src/main/java/waterfall/config/CustomUserDetailsService.java
+++ src/main/java/waterfall/config/CustomUserDetailsService.java
@@ -3,6 +3,8 @@ package waterfall.config;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +17,7 @@ import org.springframework.stereotype.Component;
 
 import waterfall.model.Role;
 import waterfall.model.User;
+import waterfall.service.LoginAttemptsService;
 import waterfall.service.UserService;
 
 @Component
@@ -22,11 +25,21 @@ public class CustomUserDetailsService implements UserDetailsService{
 	
 	@Autowired
 	private UserService userService;
-
+	
+	@Autowired
+	private LoginAttemptsService loginAttemptsService;
+	
+	@Autowired
+	private HttpServletRequest request;
+	
 	private static final Logger logger = LoggerFactory.getLogger(CustomUserDetailsService.class);
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		if (loginAttemptsService.isBlocked(getClientIP())) {
+			throw new RuntimeException("Blocked");
+	    }
+		
 		User user = userService.findByUsername(username);
 		
 		if(user == null) {
@@ -36,6 +49,16 @@ public class CustomUserDetailsService implements UserDetailsService{
 			logger.info("User has been found: {}", user);
 			return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), user.isEnabled(), true, true, true, getAuthorities(user));
 		}
+	}
+
+	private String getClientIP() {
+		String xfHeader = request.getHeader("X-Forwarded-For");
+		
+	    if (xfHeader == null){
+	        return request.getRemoteAddr();
+	    }
+	    
+	    return xfHeader.split(",")[0];
 	}
 
 	private List<GrantedAuthority> getAuthorities(User user) {
