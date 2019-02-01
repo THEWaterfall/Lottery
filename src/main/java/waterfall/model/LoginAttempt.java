97c92e385ffa5ccea51c9d013fbe8fca63623c3a
THEWaterfall <floodlight98@gmail.com>
2019-02-01 12:47:29 +0200

    Overrode equals and hashcode for LoginAttempt model

--- src/main/java/waterfall/model/LoginAttempt.java
+++ src/main/java/waterfall/model/LoginAttempt.java
@@ -64,4 +64,33 @@ public class LoginAttempt {
 		
 		return calendar.getTime();
 	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if(o == this)
+			return true;
+		if(o == null)
+			return false;
+		if(!(o instanceof LoginAttempt)) 
+			return false;
+		
+		LoginAttempt loginAttempt = (LoginAttempt) o;
+		
+		if(key == loginAttempt.getKey() &&
+		   expiryDate.equals(loginAttempt.getExpiryDate())) {
+			return true;
+		} else {
+			return false;
+		}
+		
+	}
+	
+	@Override
+	public int hashCode() {
+		final int constant = 22;
+		int result = 1;
+		result = result * constant + key.hashCode();
+		result = result * constant + expiryDate.hashCode();
+		return result;
+	}
 }
